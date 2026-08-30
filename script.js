314ec376793e68c7bf8170e4b7694fb738f9c42d
KaydenEnterprises <kayden.au2014@gmail.com>
2026-08-30 17:24:39 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-Hello
+let stackchipstock = "Instock";
+let jumpystock = "Instock";
+let skittlestock = "Instock";
+let wfstock = "Instock";
+document.getElementById("stackchipstock").textContent = stackchipstock;
+document.getElementById("jumpystock").textContent = jumpystock;
+document.getElementById("skittlestock").textContent = skittlestock;
+document.getElementById("wfstock").textContent = wfstock;
+let stockStatus = "In stock";
+
+document.getElementById("stackchipstock").onclick = function() {
+
+    if (stackchipstock === "In stock") {
+        stackchipstock = "Not in stock";
+    } else {
+        stackchipstock = "In stock";
+    }
+
+    document.getElementById("stackchipstock").textContent = stackchipstock;
+};
